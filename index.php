66c94bd2ad10b4db783c9759b14c959041360a01
soultaker-11 <tavenjonathan@gmail.com>
2026-03-02 00:36:25 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8" />
+<title>siteName</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<link rel="shortcut icon" href="img/share/favicon.ico" />
+<link href="css/style.css" rel="stylesheet" media="all" />
+<!--[if lt IE 9]>
+<script src="../js/html5shiv-printshiv.js"></script>
+<![endif]-->
+</head>
+
+<body id="">
+<div id="page">
+
+<div id="headerWrap">
+<header>
+<p id="description">サイトの説明が入ります。</p>
+<div id="logo">
+<h1><a href="index.php">siteName</a></h1>
+<!-- / #logo --></div>
+<nav id="gNav">
+<ul>
+<li class=""><a href="/">グローバルナビゲーション</a></li>
+<li class=""><a href="/">グローバルナビゲーション</a></li>
+<li class=""><a href="/">グローバルナビゲーション</a></li>
+<li class=""><a href="/">グローバルナビゲーション</a></li>
+<li class=""><a href="/">グローバルナビゲーション</a></li>
+</ul>
+<!-- / #gNav --></nav>
+<div id="hOrder">
+<div id="hOrderTel">00-0000-0000</div>
+<ul>
+<li class="pamphlet"><a href="form/inquiry.php?id=pamphlet">資料請求</a></li>
+<li class="seminar"><a href="form/inquiry.php?id=seminar">学校説明会</a></li>
+</ul>
+<!-- / #hOrder --></div>
+<!--
+<ul id="utilityNav">
+<li><a href="/">ユーティリティナビ</a></li>
+<li><a href="/">ユーティリティナビ</a></li>
+</ul>
+-->
+</header>
+<!-- / #headerWrap --></div>
+
+<div id="mainVisualWrap">
+<div id="mainVisual">
+<h1>ここに見出し</h1>
+<p>ここに説明が入ります。ここに説明が入ります。ここに説明が入ります。</p>
+<!-- / #mainVisual --></div>
+<!-- / #mainVisualWrap --></div>
+
+<div id="contents">
+<div id="main">
+<div id="topicPath"><a href="../">HOME</a> &gt; </div>
+
+<p>ここにテキストが入ります。</p>
+<p>ここにテキストが入ります。</p>
+<p>ここにテキストが入ります。</p>
+<p>ここにテキストが入ります。</p>
+<p>ここにテキストが入ります。</p>
+
+<!--
+<div id="mfOrder">
+<p>より詳しいスクールの様子を収録。
+ぜひ説明会参加前にお読みください</p>
+<div class="orderBtn"><a href="/">資料請求</a></div>
+</div>
+-->
+<!-- / #main --></div>
+
+<div id="sub">
+<div id="subOrder">
+<ul>
+<li><a href=""><img src="" width="" height="" alt=""></a></li>
+<li><a href=""><img src="" width="" height="" alt=""></a></li>
+</ul>
+<!-- / #subOrder --></div>
+
+<div id="subNavCourse">
+<h2><a href=""><img src="" width="" height="" alt=""></a></h2>
+<ul>
+<li>
+<a href="">
+<div class="pict"><img src="http://dummyimage.com/60x60/ededed/555.png" width="" height="" alt=""></div><!--
+--><div class="text">
+<p  class="arr01">メニュー01</p>
+</div>
+</a>
+</li>
+</ul>
+<!-- / #subNavCourse --></div>
+
+<div id="subNav">
+<h2><a href=""><img src="" width="" height="" alt=""></a></h2>
+<ul class="arr01">
+<li><a href="">メニューが入ります。</a></li>
+<li><a href="">メニューが入ります。</a></li>
+<li><a href="">メニューが入ります。</a></li>
+<li><a href="">メニューが入ります。</a></li>
+<li><a href="">メニューが入ります。</a></li>
+</ul>
+<!-- / #subNav --></div>
+<!-- / #sub --></div>
+
+<p class="pageTop">
+<a href="#page"><img src="" width="" height="" alt="ページトップへ戻る" /></a>
+</p>
+<!-- / #contents --></div>
+
+<div id="footerWrap">
+<footer>
+
+<div id="fNav">
+<ul>
+<li><a href="/">フッターナビ</a></li><!--
+--><li><a href="/">フッターナビ</a></li><!--
+--><li><a href="/">フッターナビ</a></li>
+</ul>
+<!-- / #fNav --></div>
+
+<div class="wrap">
+
+<div class="leftBox">
+<div id="fCompany">
+<div id="fLogo">siteName</div>
+<p>○○アカデミー<br>
+東京都港区六本木4－5－2 MW 六本木ビル2F</p>
+<!-- / #fCompany --></div>
+<!-- / .leftBox --></div>
+
+<div class="rightBox">
+<div class="wrap">
+<div id="fOrderTel">tel00-0000-0000 平日00：00～00：00</div>
+<ul>
+<li class="pamphlet"><a href="">資料請求</a></li>
+<li class="seminar"><a href="">学校説明会</a></li>
+</ul>
+<!-- / #fOrder --></div>
+<!-- / .rightBox --></div>
+<!-- / .wrap --></div>
+
+</footer>
+<!-- / #footerWrap --></div>
+<!-- / #page --></div>
+
+<script src="http://www.google.com/jsapi" ></script>
+<script>google.load("jquery", "1.7");</script>
+<script src="/js/share.js"></script>
+<script src="/js/ga.js"></script>
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="/js/selectivizr.js" charset="utf-8"></script>
+<script src="/js/nwmatcher.js" charset="utf-8"></script>
+<![endif]-->
+<!--[if IE 6]>
+<script type="text/javascript" src="/js/DD_belatedPNG_0.0.8a-min.js"></script> 
+<script>
+DD_belatedPNG.fix('img, .png_bg');
+</script> 
+<![endif]-->
+<script src="/js/jquery.tile.js" ></script>
+<script charset="utf-8">
+$(function(){
+  $(".itemList ul li.item01 .itemWrap").tile();
+});
+</script>
+
+</body>
+</html>
